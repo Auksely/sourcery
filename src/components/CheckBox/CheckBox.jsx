f3e66156e830f39f9e46bb68432a4c99a5c2de59
Auksė Četrauskaitė <66599004+Auksely@users.noreply.github.com>
2023-01-24 14:04:19 +0200

    Add files via upload

---
+++ src/components/CheckBox/CheckBox.jsx
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+import './check-box.scss';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+
+const Checkbox = ({
+  type = 'checkbox',
+  label,
+  value,
+  name,
+  onChange,
+  id,
+  error,
+}) => {
+  const [checked, setChecked] = useState(false);
+
+  const handleKeyDown = (e) => {
+    if (e.key === 'Enter') {
+      setChecked((current) => !current);
+    }
+  };
+
+  const handleClick = () => {
+    setChecked((current) => !current);
+  };
+  const checkBoxClass = classNames('checkbox__fake', {
+    'checkbox__fake--is-error': error,
+  });
+
+  return (
+    <div className="checkbox">
+      <input
+        type={type}
+        checked={checked}
+        value={value}
+        name={name}
+        onChange={onChange}
+        id={id}
+        onKeyDown={handleKeyDown}
+        onClick={handleClick}
+      />
+      <span className={checkBoxClass} />
+      <label htmlFor={id} className="checkbox__label">
+        {label}
+      </label>
+    </div>
+  );
+};
+
+export default Checkbox;
+Checkbox.propTypes = {
+  error: PropTypes.bool,
+  type: PropTypes.string,
+  label: PropTypes.string.isRequired,
+  value: PropTypes.node,
+  name: PropTypes.string,
+  onChange: PropTypes.func.isRequired,
+  id: PropTypes.string.isRequired,
+};
