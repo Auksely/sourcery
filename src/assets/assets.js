1252a50ad6cd32b028a5bced0ff8c3694f59f4e9
Auksė Četrauskaitė <66599004+Auksely@users.noreply.github.com>
2023-01-24 13:37:33 +0200

    Create assets.js

---
+++ src/assets/assets.js
@@ -0,0 +1,49 @@
+import WalkingToLaptopImg from './WalkingToLaptop.svg';
+import WorkingWithLaptop from './image-working-with-laptop.svg';
+import MacWithHammer from './image-mac-with-hammer.svg';
+import WebTechAbbr from './image-web-tech-abbr.svg';
+import WorkingAtDeskKidRobot from './image-working-at-desk-and-kid-with-robot.svg';
+import Computer404 from './computer-404.svg';
+import CordLeft from './cord-404-left.svg';
+import CordRight from './cord-404-right.svg';
+import ProcessStepOne from './image-step-1-girl-with-page.svg';
+import ProcessStepTwo from './image-step-2-man-with-envelope.svg';
+import ProcessStepThree from './image-step-3-people-with-phone.svg';
+import ProcessStepFour from './image-step-4-people-with-checkmark.svg';
+import Construction from './construction.svg';
+import Gear from './gear.svg';
+import Hammer from './hammer.svg';
+import ApplyDevelopers from './apply-developers.svg';
+import ApplyTesters from './apply-testers.svg';
+import ApplyFrontEnd from './apply-front-end.svg';
+import Paulina from './paulina.svg';
+import Giedre from './giedre.svg';
+import Arturas from './arturas.svg';
+import Aukse from './aukse.svg';
+import Linas from './linas.svg';
+
+export const assets = {
+  WalkingToLaptopImg: WalkingToLaptopImg,
+  WorkingWithLaptop: WorkingWithLaptop,
+  MacWithHammer: MacWithHammer,
+  WebTechAbbr: WebTechAbbr,
+  WorkingAtDeskKidRobot: WorkingAtDeskKidRobot,
+  Computer404: Computer404,
+  CordLeft: CordLeft,
+  CordRight: CordRight,
+  ProcessStepOne: ProcessStepOne,
+  ProcessStepTwo: ProcessStepTwo,
+  ProcessStepThree: ProcessStepThree,
+  ProcessStepFour: ProcessStepFour,
+  Construction: Construction,
+  Gear: Gear,
+  Hammer: Hammer,
+  ApplyDevelopers: ApplyDevelopers,
+  ApplyFrontEnd: ApplyFrontEnd,
+  ApplyTesters: ApplyTesters,
+  Paulina: Paulina,
+  Giedre: Giedre,
+  Arturas: Arturas,
+  Aukse: Aukse,
+  Linas: Linas,
+};
